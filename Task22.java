f1f8a693b487e3b4f2b182b82a92963de8fb2da0
SuperDiplomat <90503331+SuperDiplomat@users.noreply.github.com>
2021-09-15 14:37:32 +0300

    Add files via upload

---
+++ Task22.java
@@ -0,0 +1,19 @@
+package day5_unary_assigments_relations;
+
+public class Task22 {
+
+	public static void main(String[] args) {
+
+		int a=2, b=3;
+		boolean test= a>b;
+		System.out.println( a + " is greater than " + b + "-" + test);
+		
+		int x=5, y=3;
+		boolean test2= x>y;
+		System.out.println("First number is greater than second number- " + test2);
+		
+		
+		
+	}
+
+}
